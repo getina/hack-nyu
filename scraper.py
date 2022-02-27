5416c00033fbe44b825597a2aed21e671c9766a0
Tina Ge <61879442+getina@users.noreply.github.com>
2022-02-27 09:54:16 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,34 +12,32 @@ def sum_num():
     # response.headers.add('Access-Co ntrol-Allow-Origin', '*')
     # return response
     return {"term": "definition"}
-
-
- # Code for Actual Scraper  
+ 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
 
-# import pip._vendor.requests 
-# from bs4 import BeautifulSoup
+# Code for Actual Scraper 
+import pip._vendor.requests 
+from bs4 import BeautifulSoup
 
-# word = "alan turing"
-# word = word.replace(" ", "-")
-# first_letter = word[0]
+word = "alan turing"
+word = word.replace(" ", "-")
+first_letter = word[0]
 
-# URL = "https://www.computer-dictionary-online.org/definitions-{}/{}.html".format(first_letter, word)
-# page = pip._vendor.requests.get(URL) # *handle page does not exist
-# soup = BeautifulSoup(page.content, 'html.parser').body
-# exists = True
+URL = "https://www.computer-dictionary-online.org/definitions-{}/{}.html".format(first_letter, word)
+page = pip._vendor.requests.get(URL) # *handle page does not exist
+soup = BeautifulSoup(page.content, 'html.parser').body
+exists = True
 
-# for paragraph in soup.find_all('p'):
-#     paragraph = paragraph.get_text()
-#     if paragraph == "[Jargon File]":
-#         continue
-#     elif paragraph == "Sorry, but the page you're looking for isn't here.":
-#         exists = False
-#         break
+for paragraph in soup.find_all('p'):
+    paragraph = paragraph.get_text()
+    if paragraph == "[Jargon File]"
+        continue
+    elif paragraph == "Sorry, but the page you're looking for isn't here.":
+        exists = False
+        break
+    print(paragraph)
     
-#     print(paragraph)
-
-# if not exists:
-#     print("oh noooo")
+if not exists:
+    print("oh noooo")
 
